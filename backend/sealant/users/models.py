03c6f1fb0d5efa281c6123521895169c5a1f52a3
StudentFPW <jofeleonids00@gmail.com>
2023-12-14 22:50:34 +0200

    14/12/2023

--- backend/sealant/users/models.py
+++ backend/sealant/users/models.py
@@ -1,6 +1,37 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+# from django.conf import settings
+
 
 class User(AbstractUser):
-    pass
+    # is_client = models.BooleanField(default=False)
+    # is_service_organization = models.BooleanField(default=False)
+    # is_manager = models.BooleanField(default=False)
+    company = models.CharField(max_length=100, default="", blank=True)
+    foto = models.ImageField(upload_to="profile/", blank=True)
+    website = models.URLField(max_length=200, blank=True)
+
+    def __str__(self):
+        return f"Username: {self.username}, Email: {self.email}, Name: {self.first_name if self.first_name else 'not registered'}"
+
+
+# class Client(models.Model):
+#     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+#     def __str__(self):
+#         return self.user.username
+
+
+# class ServiceOrganization(models.Model):
+#     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+#     def __str__(self):
+#         return self.user.username
+
+
+# class Manager(models.Model):
+#     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+#     def __str__(self):
+#         return self.user.username
