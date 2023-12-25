a6f59563c102a5639d1ad37aa93d95efb9e0d51e
StudentFPW <jofeleonids00@gmail.com>
2023-12-25 23:34:31 +0200

    25/12/2023

--- backend/sealant/sealant/settings.py
+++ backend/sealant/sealant/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(os.environ.get("DEBUG", default=0))
 
 if DEBUG:
     CORS_ALLOW_ALL_ORIGINS = True
@@ -39,7 +39,7 @@ else:
         "http://127.0.0.1:3000/",
     ]
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
 
 # Application definition
 
